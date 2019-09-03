8a6c273b6ab5cc99083337fd73b4d4b607646183
pangpang <1617486564@qq.com>
2019-09-03 13:07:35 +0800

    项目框架初始配置

---
+++ demo/src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import './lib/mui/css/mui.min.css'
+import { Header } from 'mint-ui'
+import 'mint-ui/lib/style.css'
+Vue.component(Header.name, Header)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
